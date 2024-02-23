1c2bf6e7532a5216f3b67ce707cad59d5253b33e
Abner Santos <88912494+abnerpsantos@users.noreply.github.com>
2024-02-23 20:49:46 +0000

    (03-enum): Create 'defining_enum' function

--- src/bin/03-enum/main.rs
+++ src/bin/03-enum/main.rs
@@ -1,18 +1,18 @@
 fn main() {
+
+    defining_enum();
+}
+
+fn defining_enum() {
+    println!("While structs give you a way of grouping related fiedls and data");
+    println!("Enums give you a way of say a value is one of all posible values");
+
     /*
-    Where structs give you a way of grouping together related fields and data, like a Rectangle with its width and height, enums give you a way of saying a value is one of a possible set of values. 
-    For example, we may want to say that Rectangle is one of a set of possible shapes that also includes Circle and Triangle. 
-    To do this, Rust allows us to encode these possibilities as an enum.
-
-    Let’s look at a situation we might want to express in code and see why enums are useful and more appropriate than structs in this case. 
-    Say we need to work with IP addresses. 
-    Currently, two major standards are used for IP addresses: version four and version six. 
-    Because these are the only possibilities for an IP address that our program will come across, we can enumerate all possible variants, which is where enumeration gets its name.
-    Any IP address can be either a version four or a version six address, but not both at the same time. That property of IP addresses makes the enum data structure appropriate because an enum value can only be one of its variants. 
-     Both version four and version six addresses are still fundamentally IP addresses, so they should be treated as the same type when the code is handling situations that apply to any kind of IP address.
-
-    We can express this concept in code by defining an IpAddrKind enumeration and listing the possible kinds an IP address can be, V4 and V6.
+    Let's look at a situation with IP addressess
+    IP's can be V4 or V6, with enums it's easy to define it's data
      */
+
+    #[derive(Debug)]
     enum IpAddrKind {
         V4,
         V6
@@ -20,57 +20,15 @@ fn main() {
 
     let _ip_v4: IpAddrKind = IpAddrKind::V4;
     let _ip_v6: IpAddrKind = IpAddrKind::V6;
-    
-    /*
-    Note that the variants of the enum are namespaced under its identifier, and we use a double colon to separate the two. 
-    This is useful because now both values IpAddrKind::V4 and IpAddrKind::V6 are of the same type: IpAddrKind. 
-    We can then, for instance, define a function that takes any IpAddrKind:
-     */
-    fn route(_ip_kind: IpAddrKind) {}
-
-    route(IpAddrKind::V4);
-    route(IpAddrKind::V6);
 
     /*
-    Using enums has even more advantages. Thinking more about our IP address type, at the moment we don’t have a way to store the actual IP address data; we only know what kind it is. You might be tempted to tackle this problem with structs as
+    both variables are typed as IpAddrKind, so if a function receive it's data type, both variables can be used.
      */
-{
-    #[allow(dead_code)]
-    struct IpAddr {
-        kind: IpAddrKind,
-        address: String,
+
+    fn get_ip(ip: IpAddrKind) {
+        println!("{:?}", ip)
     }
 
-    let _home: IpAddr = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let _loopback: IpAddr = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-}
-
-    /*
-    Here, we’ve defined a struct IpAddr that has two fields: a kind field that is of type IpAddrKind (the enum we defined previously) and an address field of type String. 
-    We have two instances of this struct. 
-    The first is home, and it has the value IpAddrKind::V4 as its kind with associated address data of 127.0.0.1. 
-    The second instance is loopback. It has the other variant of IpAddrKind as its kind value, V6, and has address ::1 associated with it. 
-    We’ve used a struct to bundle the kind and address values together, so now the variant is associated with the value.
-
-    However, representing the same concept using just an enum is more concise: rather than an enum inside a struct, we can put data directly into each enum variant. 
-    This new definition of the IpAddr enum says that both V4 and V6 variants will have associated String values:
-     */
-
-{
-    enum IpAddr {
-        V4(String),
-        V6(String),
-    }
-
-    let _home: IpAddr = IpAddr::V4(String::from("127.0.0.1"));
-
-    let _loopback: IpAddr = IpAddr::V6(String::from("::1"));
-}
+    get_ip(_ip_v4);
+    get_ip(_ip_v6);
 }
