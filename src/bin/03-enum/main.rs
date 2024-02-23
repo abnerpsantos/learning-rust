08a8a2cc006dc45c99039806c80f3e8aef0dc38b
Abner Santos <88912494+abnerpsantos@users.noreply.github.com>
2024-02-23 21:25:57 +0000

    (03-enum): Create 'storing data in enum' function

--- src/bin/03-enum/main.rs
+++ src/bin/03-enum/main.rs
@@ -1,8 +1,10 @@
 fn main() {
 
-    defining_enum();
+    // defining_enum();
+    storing_data_in_enum();
 }
 
+#[allow(dead_code)]
 fn defining_enum() {
     println!("While structs give you a way of grouping related fiedls and data");
     println!("Enums give you a way of say a value is one of all posible values");
@@ -31,4 +33,42 @@ fn defining_enum() {
 
     get_ip(_ip_v4);
     get_ip(_ip_v6);
+}
+
+#[allow(dead_code)]
+fn storing_data_in_enum() {
+    println!("We can store data in structs as we already know...");
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let _home: IpAddr = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let _loopback: IpAddr = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    println!("but we can achieve the same behavior with enums:");
+
+    #[derive(Debug)]
+    enum IpAddrKindData {
+        V4(String),
+        V6(String)
+    }
+
+    let home: IpAddrKindData = IpAddrKindData::V4(String::from("127.0.0.1"));
+    let loopback: IpAddrKindData = IpAddrKindData::V6(String::from("::1"));
+
+    println!("{:?}", home);
+    println!("{:?}", loopback)
 }
